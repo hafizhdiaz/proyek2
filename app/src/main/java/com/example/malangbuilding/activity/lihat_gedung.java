e06442de3e4b3b58ae485131b315a19c9ede4c62
Wijip <wiji.prabowo0857@gmail.com>
2019-11-19 08:08:39 +0700

    menghapus maps

---
+++ app/src/main/java/com/example/malangbuilding/activity/lihat_gedung.java
@@ -0,0 +1,12 @@
+package com.example.malangbuilding.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.malangbuilding.R;
+
+
+
+public class lihat_gedung extends Activity {
+
+}
